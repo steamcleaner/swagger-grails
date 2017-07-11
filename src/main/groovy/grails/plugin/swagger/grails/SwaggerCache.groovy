5404b6be4ad7b8676cb7a774c133ebb50185bbea
steamcleaner <steamcleaner@users.noreply.github.com>
2017-07-11 06:54:57 -0500

    cache changes

--- src/main/groovy/grails/plugin/swagger/grails/SwaggerCache.groovy
+++ src/main/groovy/grails/plugin/swagger/grails/SwaggerCache.groovy
@@ -14,38 +14,31 @@ class SwaggerCache {
     Swagger swagger
 
     private Swagger backup = null
-    private boolean backupLive = false
+    private boolean flushed = true
 
     /**
      * Returns the already built instance of swagger if it exists.
      * <br><br>
-     * Otherwise a new instance of {@link Swagger} is built and the result of
-     * the supplied closure is stored in it.
-     * <br><br>
-     * If any errors occur while building the swagger spec, this
-     * guy will return the backup version.
      *
      * @param closure
      * @return An instance of {@link Swagger}
      */
     Swagger getOrElse(Closure closure) {
-        if (!backup || backupLive) {
+        if (flushed) {
             try {
                 closure.call(swagger)
                 backup = swagger
-                backupLive = false
             } catch (Exception e) {
-                log.error("swagger --:>  SWAGGER CACHE FAILURE -- using backup.")
                 log.error(e.getMessage(), e)
-
-                backupLive = true
                 swagger = backup
+            } finally {
+                flushed = false
             }
         }
         swagger
     }
 
     void flush() {
-        swagger = null
+        flushed = true
     }
 }
